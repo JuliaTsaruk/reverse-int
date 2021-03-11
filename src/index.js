c23e39c6fb69320b86acf3d526cfd619c5694efd
Yuliya Tsaruk <Juliamylife@yandex.ru>
2021-03-11 12:41:54 +0300

    feat: add decision

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+  let newNumber = Math.abs(n),
+      newString = String(newNumber),
+      reversedString = newString.split("").reverse().join(""),
+      reversedNumber = Number(reversedString);
+  return reversedNumber;
 }
